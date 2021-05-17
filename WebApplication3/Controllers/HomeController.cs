e968c7239e90e5d0f22fdccce6fce6312ddce3fd
mahmoudmagdy332 <mahmoudmagdy99925332@gmail.com>
2021-05-18 01:24:24 +0200

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required, StringLength(50, MinimumLength = 2), DataType(DataType.Text)]
+        public string TripTitle { get; set; }
+        [Required, StringLength(50, MinimumLength = 2), DataType(DataType.Text)]
+        public string TripDestenation { get; set; }
+        [Required, DataType(DataType.MultilineText)]
+        public string TripDetails { get; set; }
+        [Required, DataType(DataType.Upload)]
+        public string TripImage { get; set; }
+        public double Price { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime TripDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime PostDate { get; set; }
+        [DefaultValue(0)]
+        public int RankLike { get; set; }
+        [DefaultValue(0)]
+        public int RankDislike { get; set; }
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<Comment> comments { get; set; }
+        public virtual ICollection<Like> Likes { get; set; }
+        public virtual ICollection<SavePost> savePosts { get; set; }
+    }
+}
